9dfd6c5dbbaf05147b629b82c397379a3413a2e8
LewdFerret <dwayne.newsletter@gmail.com>
2024-06-22 01:48:51 +0200

    Initial Ragequit

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,5 @@
-import CustomClient from "./base/classes/CustomClient";
-
-
-const client: CustomClient = new CustomClient();
-client.Init();
-
 process.on('SIGINT', async () => {
-    await client.destroy();
+    //await client.destroy();
 
     console.log("Bot logged off.");
 
